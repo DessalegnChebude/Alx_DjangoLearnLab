dae07e9ea112b140549bd09da81ab28a3c0b18d1
DessalegnChebude <letgo900@gmail.com>
2024-11-03 16:43:01 +0300

    doing some config and setup for the admin portal

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -5,3 +5,8 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+# class Product(models.Model):
+#     product_name = models.CharField(max_length=200)
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     description = models.CharField(max_length=300)
