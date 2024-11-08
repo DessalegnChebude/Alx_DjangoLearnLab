9b3ba0de57074f6da97dd8ef572d8fe9de8203ba
DessalegnChebude <letgo900@gmail.com>
2024-11-08 22:35:12 +0300

    some update

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -6,7 +6,3 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
-# class Product(models.Model):
-#     product_name = models.CharField(max_length=200)
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     description = models.CharField(max_length=300)
