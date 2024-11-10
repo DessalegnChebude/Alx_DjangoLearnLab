373870f184391e9458b3c9df2fdcd139b76581af
DessalegnChebude <letgo900@gmail.com>
2024-11-10 09:17:53 +0300

    adding some query sets

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,33 +1,32 @@
-from relationship_app.models import Book,Library, Librarian
+# relationship_app/query_samples.py
+from relationship_app.models import Author, Book, Library
 
-# Query to get all books by a specific author
 def get_books_by_author(author_name):
-    books = Book.objects.filter(author=author_name)
-    return books
-
-# Query to get all books in a specific library
-def get_books_in_library(library_id):
-    library = Library.objects.get(name=library_name)
-    books = library.books.all()  # 'books' is the related name used in Library model
-    return books
-
-#  Retrieve the Librarian for a Library
-def get_librarian_for_library(library_id):
-    library = Library.objects.get(id=library_id)
-    librarian = library.librarian  # 'librarian' is the ForeignKey field in Library model
-    return librarian
-
-# Putting It All Together
-def get_books_by_author(author_name):
-    books = Book.objects.filter(author=author_name)
-    return books
+    try:
+        # Retrieve the Author object
+        author = Author.objects.get(name=author_name)
+        # Query all books by this specific author
+        books = Book.objects.filter(author=author)
+        return books
+    except Author.DoesNotExist:
+        return []  # Return an empty list if the author doesn't exist
 
 def get_books_in_library(library_id):
-    library = Library.objects.get(id=library_id)
-    books = library.books.all()
-    return books
+    try:
+        # Retrieve the Library object
+        library = Library.objects.get(id=library_id)
+        # List all books in the library using the ManyToManyField relationship
+        books = library.books.all()
+        return books
+    except Library.DoesNotExist:
+        return []  # Return an empty list if the library doesn't exist
 
 def get_librarian_for_library(library_id):
-    library = Library.objects.get(id=library_id)
-    librarian = library.librarian
-    return librarian
+    try:
+        # Retrieve the Library object
+        library = Library.objects.get(id=library_id)
+        # Access the related Librarian using the OneToOneField relationship
+        librarian = library.librarian
+        return librarian
+    except Library.DoesNotExist:
+        return None  # Return None if the library doesn't exist
