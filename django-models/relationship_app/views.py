c21148edec174e3ee580dd02cd2e365181be33d0
DessalegnChebude <letgo900@gmail.com>
2024-11-10 12:26:22 +0300

    working on views and urls with some templete to render

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
+
 
 # Create your views here.
+
+def list_books(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'relationship_app/book_list.html', context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+
+    context['books'] = self.objects.books.all()
+    return context
