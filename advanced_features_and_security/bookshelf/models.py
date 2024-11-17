d21d17dde6b9ef5f3255f5804899b232e59506c3
DessalegnChebude <letgo900@gmail.com>
2024-11-18 00:00:11 +0300

    d

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.CharField(max_length=100)
+#     publication_year = models.IntegerField()
 
+# from django.db import models
+
+class Document(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view document"),
+            ("can_create", "Can create document"),
+            ("can_edit", "Can edit document"),
+            ("can_delete", "Can delete document"),
+        ]
+
+    def __str__(self):
+        return self.title
--- advanced_features_and_security/bookshelf/views.py
+++ advanced_features_and_security/bookshelf/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import get_object_or_404, render, redirect
+from django.http import HttpResponseForbidden
+from .models import Document
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def document_list(request):
+    documents = Document.objects.all()
+    return render(request, 'bookshelf/document_list.html', {'documents': documents})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def document_create(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Document.objects.create(title=title, content=content)
+        return redirect('document_list')
+    return render(request, 'bookshelf/document_form.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def document_edit(request, pk):
+    document = get_object_or_404(Document, pk=pk)
+    if request.method == "POST":
+        document.title = request.POST.get('title')
+        document.content = request.POST.get('content')
+        document.save()
+        return redirect('document_list')
+    return render(request, 'bookshelf/document_form.html', {'document': document})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def document_delete(request, pk):
+    document = get_object_or_404(Document, pk=pk)
+    if request.method == "POST":
+        document.delete()
+        return redirect('document_list')
+    return render(request, 'bookshelf/document_confirm_delete.html', {'document': document})
+
