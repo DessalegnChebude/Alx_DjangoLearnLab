aa18595bb16a0f05437e9c18411b7130bc42419e
DessalegnChebude <letgo900@gmail.com>
2024-11-17 16:48:44 +0300

    adding custome user and custome user manager

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 LOGIN_REDIRECT_URL = 'list_books'  # Replace 'home' with the name of the view or URL you want users redirected to after login
 LOGOUT_REDIRECT_URL = 'login'  # Redirect users to login page after logout
 
-AUTH_USER_MODEL = 'bookshelf.CustomeUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
