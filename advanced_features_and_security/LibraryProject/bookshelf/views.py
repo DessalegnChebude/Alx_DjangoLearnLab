e686361722c1c0c392c550b5dbe1446f976156fc
DessalegnChebude <letgo900@gmail.com>
2024-11-18 00:35:01 +0300

    comit

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -66,3 +66,12 @@ def example_view(request):
     else:
         form = ExampleForm()  # Initialize an empty form
     return render(request, 'bookshelf/form_example.html', {'form': form})
+
+# bookshelf/views.py
+from django.shortcuts import render
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()  # Fetch all books from the database
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
