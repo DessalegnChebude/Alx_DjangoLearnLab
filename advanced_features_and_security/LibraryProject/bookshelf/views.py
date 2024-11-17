0adc1c116f20cc68e4074d613c2a0776d9523933
DessalegnChebude <letgo900@gmail.com>
2024-11-18 00:09:35 +0300

    df

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -10,12 +10,12 @@ from django.http import HttpResponseForbidden
 from .models import Document
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def document_list(request):
+def book_list(request):
     documents = Document.objects.all()
     return render(request, 'bookshelf/document_list.html', {'documents': documents})
 
 @permission_required('bookshelf.can_create', raise_exception=True)
-def document_create(request):
+def book_create(request):
     if request.method == "POST":
         title = request.POST.get('title')
         content = request.POST.get('content')
@@ -24,7 +24,7 @@ def document_create(request):
     return render(request, 'bookshelf/document_form.html')
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
-def document_edit(request, pk):
+def book_edit(request, pk):
     document = get_object_or_404(Document, pk=pk)
     if request.method == "POST":
         document.title = request.POST.get('title')
@@ -34,7 +34,7 @@ def document_edit(request, pk):
     return render(request, 'bookshelf/document_form.html', {'document': document})
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
-def document_delete(request, pk):
+def book_delete(request, pk):
     document = get_object_or_404(Document, pk=pk)
     if request.method == "POST":
         document.delete()
