4e53f93a26650b0dd10bdad5a33cce213b0c5ffd
DessalegnChebude <letgo900@gmail.com>
2024-11-18 13:35:17 +0300

    fix some bug on book model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,18 +1,10 @@
 from django.db import models
 
 # Create your models here.
-# class Book(models.Model):
-#     title = models.CharField(max_length=200)
-#     author = models.CharField(max_length=100)
-#     publication_year = models.IntegerField()
-
-# from django.db import models
-
-class Document(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
 
     class Meta:
         permissions = [
