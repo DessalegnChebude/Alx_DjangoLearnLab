45a943ebfa9d7aa29bbf1c80d11fc8c5aa15d2ba
DessalegnChebude <letgo900@gmail.com>
2024-12-08 21:06:15 +0300

    some bug fix

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 # from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+
 
 # Create your models here.
 
@@ -18,7 +20,7 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+    tags = TaggableManager()
     
     def __str__(self):
         return f"{self.title} by {self.author} in {self.published_date}"
