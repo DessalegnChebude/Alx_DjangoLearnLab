f162ec8b3411a044180cfc5d249cf378add70e55
DessalegnChebude <letgo900@gmail.com>
2024-12-14 13:32:04 +0300

    fix bug

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,23 +1,22 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-from accounts.models import CustomUser
+from.models import CustomUser
 
-
-class RegisterSerializer(serializers.ModelSerializer):
-    User = get_user_model()
+User = get_user_model()  # Get the custom or default User model
+class RegisterSerializer(serializers.ModelSerializer):    
     password = serializers.CharField(write_only=True, required=True)
     confirm_password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'email', 'password', 'confirm_password','bio', 'profile_picture']
         
 
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
@@ -29,6 +28,5 @@ class RegisterSerializer(serializers.ModelSerializer):
     
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['id', 'username', 'email', 'bio', 'profile_picture']
-
